94372578a8806b1849f8a25f11953a99530e7f1d
Pierre Ballif <pierre.ballif@tum.de>
2023-03-25 17:07:15 +0100

    Stacy moves

--- client/main.js
+++ client/main.js
@@ -24,7 +24,7 @@ document.body.appendChild(renderer.domElement);
 const perspective = new THREE.PerspectiveCamera(
   50,
   canvas.width / canvas.height,
-  0.1,
+  0.00001,
   1000
 );
 perspective.position.z = 30;
@@ -41,6 +41,8 @@ const stacy_mtl = new THREE.MeshPhongMaterial({
   skinning: true,
   side: THREE.DoubleSide,
 });
+
+var stacy = null;
 
 const loader = new THREE.GLTFLoader();
 new Promise((resolve, reject) => {
@@ -66,7 +68,21 @@ new Promise((resolve, reject) => {
     model.scale.set(7, 7, 7);
     model.position.y = -11;
 
+    let axesHelper = new THREE.AxesHelper( 3 );
+    model.add(axesHelper);
+
+//    model.children[0].scale.set(1, 1, 1);
+
     scene.add(model);
+
+    perspective.position.x = 10;
+//    perspective.lookAt(model.children[0]);
+
+    stacy = model.children[0];
+//    stacy.scale.set(1, 1, 1);
+
+    axesHelper = new THREE.AxesHelper( 2 );
+    stacy.add(axesHelper);
   },
   err => console.error(err)
 );
@@ -104,13 +120,8 @@ floor.receiveShadow = true;
 floor.position.y = -11;
 scene.add(floor);
 
-let geometry = new THREE.SphereGeometry(8, 32, 32);
-let material = new THREE.MeshBasicMaterial({ color: 0x9bffaf }); // 0xf2ce2e 
-let sphere = new THREE.Mesh(geometry, material);
-sphere.position.z = -15;
-sphere.position.y = -2.5;
-sphere.position.x = -0.25;
-scene.add(sphere);
+let axesHelper = new THREE.AxesHelper( 8 );
+scene.add(axesHelper);
 
 function update() {
   if (resizeRendererToDisplaySize(renderer)) {
@@ -157,3 +168,228 @@ function playModifierAnimation(from, fSpeed, to, tSpeed) {
     currentlyAnimating = false;
   }, to._clip.duration * 1000 - ((tSpeed + fSpeed) * 1000));
 }
+
+const videoElement = document.getElementsByClassName('input', 'camera')[0];
+const canvasElement = document.getElementsByClassName('output', 'camera')[0];
+const canvasCtx = canvasElement.getContext('2d');
+
+var hand_results;
+
+function makeStacyFromMediapipe(mediapipe_data){
+  let stacy = {};
+  
+}
+
+const MEDIAPIPE_TO_THREE = {
+  leftHandLandmarks: {
+    0: 'mixamorigLeftHand',
+    2: 'mixamorigLeftHandThumb2',
+    3: 'mixamorigLeftHandThumb3',
+    4: 'mixamorigLeftHandThumb4',
+    5: 'mixamorigLeftHandIndex1',
+    6: 'mixamorigLeftHandIndex2',
+    7: 'mixamorigLeftHandIndex3',
+    8: 'mixamorigLeftHandIndex4',
+    9: 'mixamorigLeftHandMiddle1',
+    10: 'mixamorigLeftHandMiddle2',
+    11: 'mixamorigLeftHandMiddle3',
+    12: 'mixamorigLeftHandMiddle4',
+  },
+  poseLandmarks: {
+    12: 'mixamorigLeftShoulder',
+    14: 'mixamorigLeftArm',
+    16: 'mixamorigLeftForeArm',
+  },
+};
+
+const connectors = {
+  central : [
+    [11, 12],
+    [23, 24],
+  ],
+  symetric: [
+    [15, 19],
+    [15, 17],
+    [15, 21],
+    [13, 15],
+    [11, 13],
+    [23, 11],
+    [23, 25],
+    [25, 27],
+    [27, 29],
+    [27, 31],
+  ]
+};
+
+let cylinder_geometry = new THREE.CylinderGeometry(0.5, 0.5);
+let cylinder_material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+let spheres = [];
+let cylinders = [];
+for(const pair of connectors.central){
+  cylinders[`${pair[0]}_${pair[1]}`] = new THREE.Mesh(cylinder_geometry, cylinder_material);
+}
+for(const pair of connectors.symetric){
+  cylinders[`${pair[0]}_${pair[1]}`] = new THREE.Mesh(cylinder_geometry, cylinder_material);
+  cylinders[`${pair[0] + 1}_${pair[1] + 1}`] = new THREE.Mesh(cylinder_geometry, cylinder_material);
+}
+
+let sphere_geometry = new THREE.SphereGeometry(0.02);
+let sphere_material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+const pr = pos => [ pos.x, pos.y, pos.z ];
+var factor = {}, offset = {};
+const to_three = pos => new THREE.Vector3( factor.x * pos.x + offset.x, -factor.y * pos.y + offset.y, -factor.z * pos.z + offset.z );
+const avg = (p1, p2) => p1.clone().add(p2).divideScalar(2);
+function set(reference, value){
+  reference.set(value.x, value.y, value.z);
+}
+
+function rotatePointTo(parent, child, target){
+//  console.log(`Rotating over ${pr(parent.position)} from ${pr(child.position)} to ${pr(target)}`)
+  const PARENT_POS = new THREE.Vector3(0, 0, 0);
+
+  let vpos = child.position.clone().sub(PARENT_POS);
+  let vtarget = target.clone().sub(PARENT_POS);
+  let cross = vpos.cross( vtarget );
+
+  let sine_of_angle = cross.length() / (vpos.length() * vtarget.length());
+  let angle = Math.asin(sine_of_angle);
+
+  let vector = cross.normalize();
+
+//  console.log(`Found axis (${[vector.x, vector.y, vector.z]}) and angle ${angle}`);
+  child.quaternion.setFromAxisAngle( vector, angle );
+}
+
+const MAX_POINTS = 100;
+var nb_points = 0;
+var sum_offset = new THREE.Vector3(0, 0, 0);
+var sum_factor = 0;
+
+function onResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  if(results.segmentationMask)
+    canvasCtx.drawImage(results.segmentationMask, 0, 0,
+                      canvasElement.width, canvasElement.height);
+
+  // Only overwrite existing pixels.
+  canvasCtx.globalCompositeOperation = 'source-in';
+  canvasCtx.fillStyle = '#00FF00';
+  canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+
+  // Only overwrite missing pixels.
+  canvasCtx.globalCompositeOperation = 'destination-atop';
+  canvasCtx.drawImage(
+      results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+  canvasCtx.globalCompositeOperation = 'source-over';
+  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+                 {color: '#00FF00', lineWidth: 4});
+  drawLandmarks(canvasCtx, results.poseLandmarks,
+                {color: '#FF0000', lineWidth: 2});
+  drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
+                 {color: '#C0C0C070', lineWidth: 1});
+  drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
+                 {color: '#CC0000', lineWidth: 5});
+  drawLandmarks(canvasCtx, results.leftHandLandmarks,
+                {color: '#00FF00', lineWidth: 2});
+  drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
+                 {color: '#00CC00', lineWidth: 5});
+  drawLandmarks(canvasCtx, results.rightHandLandmarks,
+                {color: '#FF0000', lineWidth: 2});
+  canvasCtx.restore();
+
+  const hips = bones.mixamorigHips;
+  const stacy = hips.parent;
+
+  if(offset.x === undefined && results.poseLandmarks){
+    const STACY_ARM_SIZE = bones.mixamorigLeftArm.position.length() * stacy.scale.x * stacy.parent.scale.x;
+    const arm_begin = results.poseLandmarks[23], arm_end = results.poseLandmarks[24];
+    const MEDIAPIPE_ARM_SIZE = (new THREE.Vector3( arm_end.x - arm_begin.x, arm_end.y - arm_begin.y, arm_end.z - arm_begin.z )).length();
+    let factor_thistime = STACY_ARM_SIZE / MEDIAPIPE_ARM_SIZE;
+
+    const left_foot = results.poseLandmarks[30], right_foot = results.poseLandmarks[31];
+    const make_vector = coords => new THREE.Vector3( coords.x, coords.y, coords.z );
+    const MEDIAPIPE_FLOOR = avg( make_vector(left_foot), make_vector(right_foot) );
+    const STACY_FLOOR = model.worldToLocal( stacy.localToWorld( new THREE.Vector3(0, 0, 0) ) );
+    let offset_thistime = MEDIAPIPE_FLOOR.sub(STACY_FLOOR);
+
+    sum_offset.add( offset_thistime );
+    sum_factor += factor_thistime;
+    nb_points += 1;
+    if(nb_points == MAX_POINTS){
+      offset = sum_offset.divideScalar( nb_points );
+      factor = sum_factor / nb_points;
+      console.warn('Setting offset to ', offset, ' and factor to ', factor);
+      factor = { x: factor, y: factor, z: factor };
+    }
+  }
+
+  if(results.poseLandmarks){
+    set(stacy.position,
+      avg(
+        to_three(results.poseLandmarks[23]),
+        to_three(results.poseLandmarks[24])
+      ).sub(
+        hips.position.clone().multiply( stacy.scale ).applyQuaternion( stacy.quaternion )
+      )
+    );
+    console.log('Hips: ', hips.position.clone().multiply( stacy.scale ).applyQuaternion( stacy.quaternion ) );
+    console.log('Setting Stacy to ', stacy.position);
+    console.log('(world:)', stacy.parent.localToWorld( stacy.position.clone() ));
+    console.log('(world:)', stacy.getWorldPosition( new THREE.Vector3(0, 0, 0) ));
+  }
+
+  for(let batch of [results.poseWorldLandmarks]){
+    for(let point_index in batch){
+      if(spheres[point_index] === undefined){
+        spheres[point_index] = new THREE.Mesh(sphere_geometry, sphere_material);
+        model.add(spheres[point_index]);
+      }
+      const new_coords = to_three(batch[point_index]);
+      for(let c of ['x', 'y', 'z']) spheres[point_index].position[c] = new_coords[c];
+    }
+  }
+
+  for(let [batch_name, batch] of Object.entries(MEDIAPIPE_TO_THREE)){
+    if(results[batch_name]){
+      for(let [index, boneName] of Object.entries(batch) ){
+        let position_mediapipe = to_three(results[batch_name][index]);
+/*
+        let joint_three = bones[boneName];
+        rotatePointTo(joint_three.parent, joint_three, joint_three.worldToLocal(position_mediapipe));
+*/
+//        if(boneName == 'mixamorigLeftArm') console.log('Avatar global pos:', pr(position_mediapipe));
+//        position_mediapipe = bones[boneName].worldToLocal(position_mediapipe);
+//        set(bones[boneName].position, position_mediapipe.divideScalar(7));
+//        if(boneName == 'mixamorigLeftArm') console.log('Local:', pr(bones[boneName].position));
+      }
+    }
+  }
+//  camera.stop();
+}
+
+const holistic = new Holistic({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+}});
+holistic.setOptions({
+  modelComplexity: 1,
+  smoothLandmarks: true,
+  enableSegmentation: true,
+  smoothSegmentation: true,
+  refineFaceLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+holistic.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await holistic.send({image: videoElement});
+  },
+  width: 640,
+  height: 360
+});
+camera.start();
