878963b502d2b334578dac6c6cce77adc3267188
Heidi Albarazi <56801905+wedalb@users.noreply.github.com>
2023-03-27 21:01:02 +0200

    tried adding a page

--- Flutter-project/flutter_application_1/lib/pages/home_page.dart
+++ Flutter-project/flutter_application_1/lib/pages/home_page.dart
@@ -10,6 +10,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Column(
+          children: [
+            // app bar
+            Row(
+              children: [Text("Hello")],
+            )
+
+            // search bar (search for a word or sentence to learn it)
+
+            // horizontal list -> card view
+
+            //
+          ],
+        ),
+      ),
+    );
   }
 }
