6a6d64b2968df889f7cea08ffd12fbf6d5f416b3
Heidi Albarazi <56801905+wedalb@users.noreply.github.com>
2023-03-28 18:19:18 +0200

    add google cloud tts

--- Flutter-project/flutter_application_1/lib/pages/translate_page.dart
+++ Flutter-project/flutter_application_1/lib/pages/translate_page.dart
@@ -1,17 +1,178 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_speech/speech_client_authenticator.dart';
+import 'package:google_speech/google_speech.dart';
+import 'package:path_provider/path_provider.dart';
 
-final serviceAccount = ServiceAccount.fromString(
-        '${(await rootBundle.loadString('assets/hale-function-380900-db1efafd1ef2.json'))}');
-class TranslatePage extends StatelessWidget {
+void main() {
+  runApp(MyApp());
+}
 
-  
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Audio File Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TranslatePage(),
+    );
+  }
+}
+
+class TranslatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _TranslatePageState();
+}
+
+class _TranslatePageState extends State<TranslatePage> {
+  bool recognizing = false;
+  bool recognizeFinished = false;
+  String text = '';
+
+  void recognize() async {
+    setState(() {
+      recognizing = true;
+    });
+    final serviceAccount = ServiceAccount.fromString(
+        '${(await rootBundle.loadString('./test_service_account.json'))}');
+    final speechToText = SpeechToText.viaServiceAccount(serviceAccount);
+    final config = _getConfig();
+    final audio = await _getAudioContent('test.wav');
+
+    await speechToText.recognize(config, audio).then((value) {
+      setState(() {
+        text = value.results
+            .map((e) => e.alternatives.first.transcript)
+            .join('\n');
+      });
+    }).whenComplete(() => setState(() {
+          recognizeFinished = true;
+          recognizing = false;
+        }));
+  }
+
+  void streamingRecognize() async {
+    setState(() {
+      recognizing = true;
+    });
+    final serviceAccount = ServiceAccount.fromString(
+        '${(await rootBundle.loadString('./test_service_account.json'))}');
+    final speechToText = SpeechToText.viaServiceAccount(serviceAccount);
+    final config = _getConfig();
+
+    final responseStream = speechToText.streamingRecognize(
+        StreamingRecognitionConfig(config: config, interimResults: true),
+        await _getAudioStream('test.wav'));
+
+    responseStream.listen((data) {
+      setState(() {
+        text =
+            data.results.map((e) => e.alternatives.first.transcript).join('\n');
+        recognizeFinished = true;
+      });
+    }, onDone: () {
+      setState(() {
+        recognizing = false;
+      });
+    });
+  }
+
+  RecognitionConfig _getConfig() => RecognitionConfig(
+      encoding: AudioEncoding.LINEAR16,
+      model: RecognitionModel.basic,
+      enableAutomaticPunctuation: true,
+      sampleRateHertz: 16000,
+      languageCode: 'en-US');
+
+  Future<void> _copyFileFromAssets(String name) async {
+    var data = await rootBundle.load('assets/$name');
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path + '/$name';
+    await File(path).writeAsBytes(
+        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
+  }
+
+  Future<List<int>> _getAudioContent(String name) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path + '/$name';
+    if (!File(path).existsSync()) {
+      await _copyFileFromAssets(name);
+    }
+    return File(path).readAsBytesSync().toList();
+  }
+
+  Future<Stream<List<int>>> _getAudioStream(String name) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path + '/$name';
+    if (!File(path).existsSync()) {
+      await _copyFileFromAssets(name);
+    }
+    return File(path).openRead();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Audio File Example'),
+      ),
       body: Center(
-        child: Text('This is the Translate Page'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            if (recognizeFinished)
+              _RecognizeContent(
+                text: text,
+              ),
+            ElevatedButton(
+              onPressed: recognizing ? () {} : recognize,
+              child: recognizing
+                  ? CircularProgressIndicator()
+                  : Text('Test with recognize'),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            ElevatedButton(
+              onPressed: recognizing ? () {} : streamingRecognize,
+              child: recognizing
+                  ? CircularProgressIndicator()
+                  : Text('Test with streaming recognize'),
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _RecognizeContent extends StatelessWidget {
+  final String text;
+
+  const _RecognizeContent({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'The text recognized by the Google Speech Api:',
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Text(
+            text,
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ],
       ),
     );
   }
